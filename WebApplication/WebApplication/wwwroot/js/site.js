3c4d531df93dd51dcebeb1c46e91eee956f419e5
Alexanderpinon <anderspin99@icloud.com>
2022-08-08 17:51:23 -0700

    added website

---
+++ WebApplication/WebApplication/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+// Functions to be called onmouseover and onmouseout to change the color text of Hello, World
+function newColor() {
+    document.getElementById("hello_world").style.color = "blue";
+}
+
+function oldColor() {
+    document.getElementById("hello_world").style.color = "darkolivegreen";
+}
