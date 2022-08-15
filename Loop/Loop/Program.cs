ddd7fbe2c0860707e5a1ec7f5e12cd72ce917d9f
Alexanderpinon <anderspin99@icloud.com>
2022-08-15 16:36:48 -0700

    loop

---
+++ Loop/Loop/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
+
+            do
+            {
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62, try again");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 29:
+                        Console.WriteLine("You guessed 29, try again");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 55:
+                        Console.WriteLine("You guessed 55, try again");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 12:
+                        Console.WriteLine("You guessed the number 12, that is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong, try again");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+            }
+            while (!isGuessed);
+
+            Console.Read();
+
+                }
+            }
+        }
+    
+
