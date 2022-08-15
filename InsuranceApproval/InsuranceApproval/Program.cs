a70440e60d84f186c826cadddeb4c4dc1de92083
Alexanderpinon <anderspin99@icloud.com>
2022-08-15 15:17:11 -0700

    fixed

--- InsuranceApproval/InsuranceApproval/Program.cs
+++ InsuranceApproval/InsuranceApproval/Program.cs
@@ -10,20 +10,32 @@ namespace InsuranceApproval
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Insurance Approval");
+           
 
-            Console.WriteLine("What is your age?");
-            int ageNumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Insurance Approval");
 
-            Console.WriteLine("Have you ever had a DUI");
-            bool hadDui = Convert.ToBoolean(Console.ReadLine());
+                Console.WriteLine("What is your age?");
+                int ageNumber = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("How many speeding tickets do you have");
-            int speedTickets = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Have you ever had a DUI, Please say true or false");
+                bool hadDui = Convert.ToBoolean(Console.ReadLine());
 
-            Console.WriteLine("Qualified?");
+                Console.WriteLine("How many speeding tickets do you have");
+                int speedTickets = Convert.ToInt32(Console.ReadLine());
 
+                Console.WriteLine("Qualified?");
 
+            if (ageNumber > 15 && hadDui == false && speedTickets <= 3)
+            {
+                Console.WriteLine("You Qualify");
+            }
+            else
+            {
+                Console.WriteLine("You do not Qualify");
+            }
+            Console.ReadLine();
+
+            }
         }
     }
-}
+
