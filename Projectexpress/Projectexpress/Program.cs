e2ea748260572e52ea4a736d61c46d339699b09f
Alexanderpinon <anderspin99@icloud.com>
2022-08-15 15:50:31 -0700

    package express

---
+++ Projectexpress/Projectexpress/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projectexpress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Welcome to Package Express. Please follow the instuctions below.");
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter the package weight:");
+            int packWeight = Convert.ToInt32(Console.ReadLine());
+
+           
+
+            if (packWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day");
+            }
+            else
+            {
+                    Console.WriteLine("Please enter package Width:");
+                    int packWidth = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Please enter package Height:");
+                    int packHeight = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Please enter package Length:");
+                    int packLength = Convert.ToInt32(Console.ReadLine());
+
+                if (packWidth > 50 && packHeight > 50 && packLength > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+                else
+                {
+
+                    int products = packWidth * packHeight * packLength * packWeight / 100;
+                    Console.WriteLine("Your Quote is " + "$" + products +  " Thank You Have a good day");
+                }
+            }
+            Console.ReadLine();
+            
+            
+            {
+
+            }
+        }
+    }
+}
