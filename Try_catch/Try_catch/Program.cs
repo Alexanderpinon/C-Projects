39acaa54cdad9c48edd11a49ce18a7ffc9a48f5a
Alexanderpinon <anderspin99@icloud.com>
2022-08-17 16:29:55 -0700

    added try_catch

---
+++ Try_catch/Try_catch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intergers = new List<int>() { 1, 3, 4, 5, 6, 7, 8 };
+            Console.WriteLine("Pick a number to Divide ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                foreach (int X in intergers)
+                {
+                    int results = X / number;
+                    Console.WriteLine(results);
+                }
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number. ");
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please dont divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
