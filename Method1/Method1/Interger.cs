b2dee2744616dd15cfa8ef999beea673b6a9bbbd
Alexanderpinon <anderspin99@icloud.com>
2022-08-22 20:33:20 -0700

    method

---
+++ Method1/Method1/Interger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method1
+{
+    class Interger
+    {
+        
+        
+        public int Mathoperation(int a)
+        {
+            int result = a + 2;
+            return result;
+        }
+        public int subtraction(int a)
+        {
+            int result = a - 3;
+            return result;
+        }
+        public int division(int a)
+        {
+            int result = a / 2;
+            return result;
+        }
+
+       
+    
+    }
+}
---
+++ Method1/Method1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Interger x = new Interger();
+            int a;
+
+            Console.WriteLine("Input Number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            int result = x.Mathoperation(a);
+            Console.Write(result);
+            Console.ReadLine();
+
+            result = x.subtraction(a);
+            Console.Write(result);
+            Console.ReadLine();
+
+            result = x.division(a);
+            Console.Write(result);
+            Console.ReadLine();
+        }
+        
+    }
+}
