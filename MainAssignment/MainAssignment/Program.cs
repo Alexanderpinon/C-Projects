afedd85892b83b606eafab4521eaa834492696f2
Alexanderpinon <anderspin99@icloud.com>
2022-08-26 18:08:50 -0700

    method assignment

---
+++ MainAssignment/MainAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            interger X = new interger();
+            Console.WriteLine("Input first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input second number:");
+            
+
+            try
+            {
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                X.Mathoperation(num1, num2);
+            }
+            catch
+            {
+                X.Mathoperation(num1);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ MainAssignment/MainAssignment/interger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainAssignment
+{
+    class interger
+    {
+        public int Mathoperation(int a, int b = 10)
+        {
+            int outcome = a + b;
+            Console.WriteLine(outcome);
+            return outcome;
+        }
+    }
+}
