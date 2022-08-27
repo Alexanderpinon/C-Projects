0c20278b04eff3517e601c33e4161fcc77c872dc
Alexanderpinon <anderspin99@icloud.com>
2022-08-27 01:17:40 -0700

    interface assignment

---
+++ PolyMorphism1/PolyMorphism1/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyMorphism1
+{
+    public class Employee : person, IQuittable
+    {
+        public void SayName()
+        {
+            Console.WriteLine("Name " + firstName + lastName);
+        }
+        public void Quit()
+        {
+
+        }
+    }
+}
---
+++ PolyMorphism1/PolyMorphism1/person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyMorphism1
+{
+    public class person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + lastName);
+        }
+    }
+}
