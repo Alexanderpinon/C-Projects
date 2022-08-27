09696cacb0dd341124d61a363da5b0a0ab4841c2
Alexanderpinon <anderspin99@icloud.com>
2022-08-26 19:02:51 -0700

    voidmethod

---
+++ Voidmethod/Voidmethod/Interger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voidmethod
+{
+    class Interger
+    {
+            public void count(int x , int y)
+            {
+                int outcome = x + 6;
+                Console.WriteLine(y);
+            }
+        
+    }
+}
---
+++ Voidmethod/Voidmethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voidmethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Interger X = new Interger();
+
+            Console.WriteLine("Input First number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            X.count(num1, num2);
+            Console.ReadLine();
+
+            
+        }
+        
+    }
+}
