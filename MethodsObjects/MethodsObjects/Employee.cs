45b459e6a144885d7cb04602249d80d12b76186b
Alexanderpinon <anderspin99@icloud.com>
2022-08-27 00:36:39 -0700

    methodsobjects

---
+++ MethodsObjects/MethodsObjects/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsObjects
+{
+    public class Employee : Person
+    {
+        public int EmployeeId { get; set; }
+    }
+}
---
+++ MethodsObjects/MethodsObjects/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee();
+            person.FirstName = "Sample";
+            person.LastName = " Student";
+            person.SayName();
+            Console.ReadLine();
+        }
+    }
+}
