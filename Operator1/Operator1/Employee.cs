1cfa4c916b26c95fa03c0b0105449661e1f77b38
Alexanderpinon <anderspin99@icloud.com>
2022-08-27 01:32:52 -0700

    operator

---
+++ Operator1/Operator1/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator1
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int EmployeeID { get; set; }
+
+        public static Employee operator== (Employee id, Employee id)
+        {
+            Employee.id.Equal(EmployeeId);
+            return Employee;
+        }
+    }
+}
