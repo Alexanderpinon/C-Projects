d57694010253bb78a5733d44d7cdaa4d22546fbd
Alexanderpinon <anderspin99@icloud.com>
2022-08-27 00:52:43 -0700

    abstract

---
+++ Abstract/Abstract/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Employee = new Employee();
+            Employee.firstName = "Sample";
+            Employee.lastName = "Student";
+            Employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
