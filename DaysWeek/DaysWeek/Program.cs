1f6ff1c979a104ac04e23313cf61cb4cb562f671
Alexanderpinon <anderspin99@icloud.com>
2022-08-27 21:07:00 -0700

    days of week

---
+++ DaysWeek/DaysWeek/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysWeek
+{
+    class Program
+    {
+        enum Dayofweek
+        {
+            none,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            string value = "Friday";
+            Dayofweek day ;
+            Console.WriteLine("Enter current day of week");
+            Console.ReadLine();
+            try
+            {
+                day = (Dayofweek)Enum.Parse(typeof(Dayofweek), value);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of week");
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+
+                day = Dayofweek.Monday;
+            }
+            if (day == Dayofweek.Friday)
+            {
+            }
+            
+        }
+    }
+       
+}
+
+           
+         
+
+
+
+        
+
