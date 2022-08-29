f740eba966a090494db777cf12088c6f9586e3c5
Alexanderpinon <anderspin99@icloud.com>
2022-08-29 15:58:46 -0700

    made operator 2

---
+++ Operator2/Operator2/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator2
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int EmployeeID { get; set; }
+        
+        public static bool operator == (Employee empOne, Employee empTwo)
+
+        {
+           return empOne.EmployeeID == empTwo.EmployeeID;
+        }
+        public static bool operator != (Employee empOne, Employee empTwo)
+        {
+            return empOne.EmployeeID != empTwo.EmployeeID;
+        }
+    }
+}
---
+++ Operator2/Operator2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate employee one and two
+            Employee empOne = new Employee() { FirstName = "Alex", LastName = "Pinon", EmployeeID = 222 }; 
+            Employee empTwo = new Employee() { FirstName = "Maya", LastName = "Shepard", EmployeeID = 333 };
+            // made a if statment to compare them
+            if (empOne == empTwo)
+            {
+                Console.WriteLine("They are equal!");
+            }
+            else
+            {
+                Console.WriteLine("They are not equal!");
+            }
+            if (empOne != empTwo)
+            {
+                Console.WriteLine("They are not equal!");
+            }
+            else
+            {
+                Console.WriteLine("They are equal!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
