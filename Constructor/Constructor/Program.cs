a3b115499843ce7cd8619bab4959c4fa849b2895
Alexanderpinon <anderspin99@icloud.com>
2022-08-29 22:28:19 -0700

    constructor

---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        const int a = 10;
+        
+        static void Main(string[] args)
+        {
+            const int b = 20;
+            const int c = b + a;
+            Console.WriteLine(c);
+            Console.ReadLine();
+
+            var age = 22;
+            Console.WriteLine("var holds an int = {0}", age);
+
+            public class mySampleClass
+        {
+            public mySampleClass()
+            {
+                // no parameter constructor method.// First Constructor  
+            }
+            public mySampleClass(int Age)
+            {
+                // constructor with one parameter.// Second Constructor  
+            }
+
+            public mySampleClass(int Age, string Name)
+        }
+    }
+}
---
+++ Constructor/Constructor/sample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class sample
+    {
+        public class mySampleClass
+        {
+            public mySampleClass()
+            {
+
+            }
+            public mySampleClass(int Age)
+            {
+
+            }
+
+        }
+    }
+}
