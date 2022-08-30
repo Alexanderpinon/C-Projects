6ff7f1d5d3063c5fffaf47294f4995b867422b28
Alexanderpinon <anderspin99@icloud.com>
2022-08-30 11:58:23 -0700

    resubmission

--- Datetime/Datetime/Program.cs
+++ Datetime/Datetime/Program.cs
@@ -10,13 +10,19 @@ namespace Datetime
     {
         static void Main(string[] args)
         {
+            // here i am putting the time it is now 
             Console.Write("Current Date and Time is : ");
             DateTime now = DateTime.Now;
             Console.WriteLine(now);
             Console.ReadLine();
 
-            Console.WriteLine("Number for time in x hours?");
-            DateTime dateTimeTime = Convert.ToDateTime(Console.ReadLine());
+            // here i am asking the user for a number for time in x hours
+             Console.WriteLine("Number for time in x hours?");
+             int X = Convert.ToInt32(Console.ReadLine());
+             DateTime date2 = now.AddHours(X);
+             Console.WriteLine("DateTime after operation: {0} ", date2);
+             Console.ReadLine();
+
         }
     }
 }
