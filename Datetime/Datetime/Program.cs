c864a552e3e011dde3cb03472528615dcf115dad
Alexanderpinon <anderspin99@icloud.com>
2022-08-29 21:46:35 -0700

    datetime

---
+++ Datetime/Datetime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Current Date and Time is : ");
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            Console.ReadLine();
+
+            Console.WriteLine("Number for time in x hours?");
+            DateTime dateTimeTime = Convert.ToDateTime(Console.ReadLine());
+        }
+    }
+}
