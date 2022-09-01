4d92ea7592f3b9cd22365544a6276d35f73abb1b
Alexanderpinon <anderspin99@icloud.com>
2022-08-31 21:33:48 -0700

    XYZSchool

---
+++ XyzSchool/XyzSchool/Grade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XyzSchool
+{
+    public class Grade
+    {
+        public int GradeId { get; set; }
+        public string GradeName { get; set; }
+        public string Section { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+
+
+}
---
+++ XyzSchool/XyzSchool/Program.cs
@@ -0,0 +1,19 @@
+﻿using XyzSchool;
+
+namespace XyzSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
