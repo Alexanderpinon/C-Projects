c017e2f64efdfcaed06448fade909a64f6ffd46f
Alexanderpinon <anderspin99@icloud.com>
2022-09-08 18:20:05 -0700

    Xyz

---
+++ XyzSchool/XyzSchool/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XyzSchool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+                Console.WriteLine(stud);
+                Console.ReadLine();
+            }
+            
+
+        }
+    }
+}
---
+++ XyzSchool/XyzSchool/SchoolContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XyzSchool
+{
+    internal class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
